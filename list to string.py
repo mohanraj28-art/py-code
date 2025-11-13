5938efeda848ea1cb0f875e28c10a93b61acba67
mohan <mohanrajdhoni71@gmail.com>
2025-11-13 18:55:40 +0530

    string to list

---
+++ list to string.py
@@ -0,0 +1,15 @@
+#list to string
+
+list=['mohan','raj','from','mechaincal']
+string=' '.join(list)
+print(f'list  to string: {string}')
+
+print(type(string))
+
+
+
+#string to list
+
+string= input("Enter a string: ")
+txt=list(string)
+print(f'list to string: {txt}')
